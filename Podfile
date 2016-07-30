45bfca3b086ecec914463c55e3913331edec23aa
huachen <huachen206@163.com>
2016-07-30 15:37:00 +0800

    first update

---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '7.0'
+#use_frameworks!
+inhibit_all_warnings!
+
+pod 'Masonry', '~> 0.6.1'
+pod 'HCKitProject', :git => 'https://github.com/huachen206/HCKitProject.git'
+
---
+++ RoadRecorder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RoadRecorder
+//
+//  Created by HuaChen on 16/7/30.
+//  Copyright © 2016年 HuaChen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
